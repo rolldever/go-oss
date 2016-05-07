f75738fb8a9082d5e97b7f1409b1c84cf0bc6d52
Eric Chai <flig@me.com>
2016-05-07 11:48:36 +0800

    Bucket 现支持获取给定对象的 HTTPS 链接。

--- bucket.go
+++ bucket.go
@@ -2,6 +2,7 @@ package oss
 
 import (
 	"fmt"
+	"strings"
 )
 
 var (
@@ -34,6 +35,25 @@ func NewPublicBucket(name, datacenter string) *Bucket {
 
 // GetObjectURL 获取给定的 Object 的 URL.
 func (b *Bucket) GetObjectURL(objectName string) string {
-	return fmt.Sprintf("https://%s.%s.%s/%s",
-		b.Name, b.Datacenter, OSSServerDomainName, objectName)
+	return b.getObjectURL(objectName, false)
+}
+
+// GetObjectURLHTTPS 获取给定的 Object 的 URL (使用 HTTPS 协议).
+func (b *Bucket) GetObjectURLHTTPS(objectName string) string {
+	return b.getObjectURL(objectName, true)
+}
+
+func (b *Bucket) getObjectURL(objectName string, usingHTTPS bool) string {
+	if strings.HasPrefix(objectName, "/") {
+		objectName = objectName[1:]
+	}
+
+	var protocol string
+	if usingHTTPS {
+		protocol = "https"
+	} else {
+		protocol = "http"
+	}
+	return fmt.Sprintf("%s://%s.%s.%s/%s",
+		protocol, b.Name, b.Datacenter, OSSServerDomainName, objectName)
 }
